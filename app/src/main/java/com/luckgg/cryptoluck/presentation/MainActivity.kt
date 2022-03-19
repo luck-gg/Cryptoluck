1c44eae3f0103c8f585f4b6a3536fac4031f8cf8
lxnek <lx.neko@gmail.com>
2022-03-19 00:21:52 -0300

    Initial commit

---
+++ app/src/main/java/com/luckgg/cryptoluck/presentation/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.luckgg.cryptoluck.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.luckgg.cryptoluck.ui.theme.CryptoluckTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoluckTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CryptoluckTheme {
+        Greeting("Android")
+    }
+}
