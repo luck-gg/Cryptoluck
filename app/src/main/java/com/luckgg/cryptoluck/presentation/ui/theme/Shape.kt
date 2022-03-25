b2767fc3e3f66b5ab963b3383fe2d47b3eabff83
lxnek <lx.neko@gmail.com>
2022-03-25 02:11:34 -0300

    0.5 Set new font type

--- app/src/main/java/com/luckgg/cryptoluck/presentation/ui/theme/Shape.kt
+++ app/src/main/java/com/luckgg/cryptoluck/presentation/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.luckgg.cryptoluck.ui.theme
+package com.luckgg.cryptoluck.presentation.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
